3d6fa4a33c1ef9152000dafd7868381b1e541e25
KimGiMoon <KimGiMoon@DESKTOP-M8E3N0A>
2020-01-07 16:38:03 +0900

    base 화면 작업

--- Intro/src/main/java/com/marin/ui/Controller/IntroController.java
+++ Intro/src/main/java/com/marin/ui/Controller/IntroController.java
@@ -1,14 +1,20 @@
 package com.marin.ui.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.marin.ui.Repository.MarinTemplate;
+
 @Controller
 @RequestMapping("/intro")
 public class IntroController {
 
+	@Autowired
+	MarinTemplate rest;
+	
 	@GetMapping
 	public ModelAndView getIntro() {
 		return new ModelAndView("baseTemplate");
---
+++ Intro/src/main/java/com/marin/ui/Repository/MarinTemplate.java
@@ -0,0 +1,8 @@
+package com.marin.ui.Repository;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.web.client.RestOperations;
+@Repository
+public interface MarinTemplate extends RestOperations{
+
+}
