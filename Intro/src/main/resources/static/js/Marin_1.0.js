c5f230efea5ad3810e03741707a81728eff48bd8
KimGiMoon <KimGiMoon@DESKTOP-M8E3N0A>
2020-01-10 18:01:56 +0900

    커밋

--- Intro/src/main/resources/static/js/Marin_1.0.js
+++ Intro/src/main/resources/static/js/Marin_1.0.js
@@ -11,3 +11,40 @@ $('.info').click(function(){
 $('.login').click(function(){
 	location.href= company+"/login";
 })
+			
+			$('.header-menu-item').mouseover(function(){
+				$(this).animate({
+ 					fontSize: '20px'
+				},"fast")
+			}) 
+			$('.header-menu-item').mouseout(function(){
+				$(this).animate({
+ 					fontSize: '1em'
+				},"fast")
+			})
+			$('.header-menu-item').click(function(){
+				$('#header').animate({
+					height : '300'
+				},"slow",function(){
+					$('.div-box-cover-top').animate({
+						right : 2000
+					},"slow",function(){
+						/*window.setTimeout(window.location.href="./login", 2000);*/
+					})
+				})
+			})
+			
+$('#login').click(function(){
+	$('#header').animate({
+		height : '300'
+	},"slow",function(){
+		$('.div-box-cover-top').animate({
+			right : 2000
+		},"slow",function(){
+			window.location.href="./login"
+			/*location.replace("./login")*/
+		})
+	})
+})
+
+/*.div-box-cover-top*/
